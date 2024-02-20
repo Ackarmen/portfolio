f1c0abf36578a05852d073b3f16d925aef842639
Ackarmen <lyaforis@gmail.com>
2024-02-20 15:53:34 +0300

    fix issue with react-icons

--- components/ThemeSwitch.tsx
+++ components/ThemeSwitch.tsx
@@ -1,18 +1,17 @@
-"use client";
+'use client';
 
-import { useTheme } from "@/context/ThemeContextProvider";
-import React from "react";
-import { BsMoon, BsSun } from "react-icons/Bs";
+import { useTheme } from '@/context/ThemeContextProvider';
+import { BsMoon, BsSun } from 'react-icons/bs';
 
 export default function ThemeSwitch() {
-	const {theme, toggleTheme} = useTheme()
+  const { theme, toggleTheme } = useTheme();
 
-	return (
-		<button
-			className="fixed bottom-5 right-5 bg-gray-300 w-[3rem] h-[3rem] bg-opacity-80 backdrop:blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
-			onClick={toggleTheme}
-		>
-			{theme === "light" ? <BsMoon /> : <BsSun />}
-		</button>
-	);
+  return (
+    <button
+      className="fixed bottom-5 right-5 bg-gray-300 w-[3rem] h-[3rem] bg-opacity-80 backdrop:blur-[0.5rem] border border-white border-opacity-40 shadow-2xl rounded-full flex items-center justify-center hover:scale-[1.15] active:scale-105 transition-all dark:bg-gray-950"
+      onClick={toggleTheme}
+    >
+      {theme === 'light' ? <BsMoon /> : <BsSun />}
+    </button>
+  );
 }
