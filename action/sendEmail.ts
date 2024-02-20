55af3760e8a978af6c69cf2c28207fe2a2a9ad0e
Ackarmen <lyaforis@gmail.com>
2024-02-20 17:02:30 +0300

    configure Resend

--- action/sendEmail.ts
+++ action/sendEmail.ts
@@ -1,46 +1,46 @@
-"use server";
+'use server';
 
-import React from "react";
-import { Resend } from "resend";
-import { validateString, getErrorMessage } from "@/lib/utils";
-import ContactFormEmail from "@/email/ContactFormEmail";
+import ContactFormEmail from '@/email/ContactFormEmail';
+import { getErrorMessage, validateString } from '@/lib/utils';
+import React from 'react';
+import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendEmail = async (formData: FormData) => {
-	const message = formData.get("message");
-	const senderEmail = formData.get("senderEmail");
+  const message = formData.get('message');
+  const senderEmail = formData.get('senderEmail');
 
-	if (!validateString(senderEmail, 500)) {
-		return {
-			error: "Invalid sender email",
-		};
-	}
-	if (!validateString(message, 5000)) {
-		return {
-			error: "Invalid message",
-		};
-	}
+  if (!validateString(senderEmail, 500)) {
+    return {
+      error: 'Invalid sender email',
+    };
+  }
+  if (!validateString(message, 5000)) {
+    return {
+      error: 'Invalid message',
+    };
+  }
 
-	let data;
-	try {
-		data = await resend.emails.send({
-			from: "Contact form <onboarding@resend.dev>",
-			to: "lyaforis@gmail.com",
-			subject: "Message from contact form",
-			reply_to: senderEmail as string,
-			react: React.createElement(ContactFormEmail, {
-				message: message as string,
-				senderEmail: senderEmail as string,
-			})
-		});
-	} catch (error: unknown) {
-		return {
-			error: getErrorMessage(error),
-		};
-	}
+  let data;
+  try {
+    data = await resend.emails.send({
+      from: 'Contact form <onboarding@resend.dev>',
+      to: 'lyaforis@gmail.com',
+      subject: 'Message from contact form',
+      reply_to: senderEmail as string,
+      react: React.createElement(ContactFormEmail, {
+        message: message as string,
+        senderEmail: senderEmail as string,
+      }),
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
 
-	return {
-		data,
-	}
+  return {
+    data,
+  };
 };
