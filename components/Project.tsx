28faedf0b00237f0991bd4bdb75275dccf6efe0f
Ackarmen <lyaforis@gmail.com>
2024-02-21 17:43:20 +0300

    optimize img

--- components/Project.tsx
+++ components/Project.tsx
@@ -71,7 +71,7 @@ export default function Project({
           quality={95}
           width={400}
           height={400}
-          className="absolute hidden sm:block top-8 -right-40 w-[28.25rem] rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-40"
+          className="w-96 absolute hidden sm:block top-8 -right-24  rounded-t-lg shadow-2xl transition group-hover:scale-[1.04] group-hover:-translate-x-3 group-hover:translate-y-3 group-hover:-rotate-2 group-even:group-hover:translate-x-3 group-even:group-hover:translate-y-3 group-even:group-hover:rotate-2 group-even:right-[initial] group-even:-left-24"
         />
       </section>
     </motion.div>
