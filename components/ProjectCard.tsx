33679170f1499acc5713f6a76375a41824f82e40
Ackarmen <lyaforis@gmail.com>
2024-02-23 18:24:38 +0300

    modify layout

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -22,28 +22,29 @@ export function ProjectCard({
         <CardBody className="bg-gray-100 relative group/card dark:hover:shadow-2xl dark:hover:shadow-yellow-500/[0.1] dark:bg-gray-700/55 dark:border-white/[0.2] border border-black/[0.1] w-auto sm:w-[30rem] h-[35rem] rounded-xl p-6">
           <CardItem
             translateZ="50"
-            className="text-xl font-bold text-neutral-600 dark:text-white"
+            className="text-xl font-bold text-neutral-600 dark:text-white mb-5"
           >
             {title}
           </CardItem>
           <CardItem
             as="p"
             translateZ="60"
-            className="text-neutral-500 text-sm max-w-sm mt-2 dark:text-neutral-300"
+            className="text-neutral-500 text-sm max-w-sm mt-2 mb-5 dark:text-neutral-300"
           >
             {description}
           </CardItem>
           <CardItem translateZ="100" className="w-full mt-4">
             <Image
+              priority
               src={imageUrl}
               height="1000"
               width="1000"
-              className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
+              className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl mb-5"
               alt="Project I worked on"
             />
             <div className="mt-4">
               <CardItem>
-                <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto">
+                <ul className="flex flex-wrap mt-4 gap-2 sm:mt-auto mb-14">
                   {tags.map((tag, index) => (
                     <li
                       className="bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full dark:text-white/70"
